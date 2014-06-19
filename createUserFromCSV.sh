380b6841b87646b69d56d22d989b24b159003e49
Sam Fortuna <sam.fortuna@jamfsoftware.com>
2014-06-19 12:47:26 -0500

    Import user objects from CSV for 9.3+

---
+++ createUserFromCSV.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+########################################################################################################
+#
+# Copyright (c) 2014, JAMF Software, LLC.  All rights reserved.
+#
+#       Redistribution and use in source and binary forms, with or without
+#       modification, are permitted provided that the following conditions are met:
+#               * Redistributions of source code must retain the above copyright
+#                 notice, this list of conditions and the following disclaimer.
+#               * Redistributions in binary form must reproduce the above copyright
+#                 notice, this list of conditions and the following disclaimer in the
+#                 documentation and/or other materials provided with the distribution.
+#               * Neither the name of the JAMF Software, LLC nor the
+#                 names of its contributors may be used to endorse or promote products
+#                 derived from this software without specific prior written permission.
+#
+#       THIS SOFTWARE IS PROVIDED BY JAMF SOFTWARE, LLC "AS IS" AND ANY
+#       EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#       WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#       DISCLAIMED. IN NO EVENT SHALL JAMF SOFTWARE, LLC BE LIABLE FOR ANY
+#       DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#       (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#       LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#       ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#       (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#       SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+####################################################################################################
+#
+# DESCRIPTION
+#
+#	This script reads a CSV of usernames and email addresses and imports them to the JSS.
+#	The format must be username in the first column and email address in the second
+#
+####################################################################################################
+#
+# HISTORY
+#
+#	Version 1.0
+#  	Created by Sam Fortuna, JAMF Software, LLC, on June 18th, 2014
+#
+####################################################################################################
+
+#Variables
+file="/path/to/file.csv"					#Path to the CSV
+server="your.jss.com"						#Server name
+username="admin"							#JSS username with API privileges
+password="password"							#Password for the JSS account
+
+#Option to read in the path from Terminal
+if [[ "$file" == "" ]]; then
+	echo "Please enter the path to the CSV"
+	read file
+fi
+
+#Verify we can read the file
+data=`cat $file`
+if [[ "$data" == "" ]]; then
+	echo "Unable to read the file path specified"
+	echo "Ensure there are no spaces and that the path is correct"
+	exit 1
+fi
+
+#Find how many users to import
+users=`echo "$data" | wc -l | awk '{print $NF}'`
+
+#Set a counter for the loop
+counter="0"
+
+#Loop through the CSV and submit data to the API
+while [ $counter -lt $users ]
+do
+	counter=$[$counter+1]
+	line=`echo "$data" | head -n $counter | tail -n 1`
+	user=`echo "$line" | awk -F , '{print $1}'`
+	email=`echo "$line" | awk -F , '{print $2}'`
+	
+	echo "$user : $email"
+	
+	#Construct the XML
+	apiData="<user><name>${user}</name><email>${email}</email></user>"
+	curl -k -v -u $username:$password -H "Content-Type: text/xml" https://$server:8443/JSSResource/users/id/id -d "$apiData" -X POST
+done
+
+exit 0
